cdfb6590e1dd0fed96f25dd375219980bceec76c
ElsyNarvaez12 <111091245+ElsyNarvaez12@users.noreply.github.com>
2024-05-30 17:22:22 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const book = document.getElementById('book');
+    const cover = document.getElementById('cover');
+    cover.addEventListener('click', function() {
+        book.classList.toggle('open');
+       
+
+
+});
+    });
+
+
+
